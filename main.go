83815d3085a578767a1ebab15a7556674d945a4b
jackson <RyanTokManMokMTM@hotmail.com>
2021-10-12 06:40:50 -0700

    feteched combine all movies and genre

--- main.go
+++ main.go
@@ -2,25 +2,124 @@ package main
 
 import (
 	"WebCrawler/webCrawler"
+	//"WebCrawler/webCrawler"
 	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"log"
 	"strconv"
 )
+
+/*
+	TODO - SAVE ALL REQUIRED DATA TO DATABASE(IN PROGRESS)
+	TODO -  MOVIE DATA FROM API AND SAVING TO DATABASE
+	MovieData Response Format :
+	adult	false -> Bool
+	backdrop_path	null -> URL
+	genre_ids	[] -> a list of genre ids  -> use another table to save those information
+	id	883725 -> primary key of movie id
+	original_language	"en" -> string
+	original_title	"Motions" -> string
+	overview	""
+	popularity	0
+	poster_path	null
+	release_date	"2021-04-06"
+	title	"Motions"
+	video	false
+	vote_average	0
+	vote_count	0
+
+	TODO -  GENRE DATA FROM API -/genre/movie/list (Genre table -> one to many movie info)
+	Genre Data Format :
+	id - int
+	name - string
+
+	TODO -  ACTOR DATA FROM API AND SAVING TO DATABASE
+	-PEOPLE RESPONSE DATA
+	adult	- bool
+	gender	- int
+	id	- int
+	known_for - [KnownFor]
+	known_for_department- string
+	name - string
+	popularity	- float
+	profile_path	- string
+
+	-PEOPLE KnownFor RESPONSE
+	backdrop_path - string
+	first_air_date	- string
+	genre_ids - []int
+	id	- int
+	media_type	- string
+	name - string
+	origin_country - []string
+	original_language	- string
+	original_name	- string
+	overview	- string
+	poster_path	- string
+	vote_average	- float
+	vote_count	- int
+
+*/
 
 const (
 	host string = "https://api.themoviedb.org/3"
 	apiKey string = "29570e7acc52b3e085ab46f6a60f0a55"
 	upcomingURI = "/movie/upcoming"
+	allMovieURI string ="/discover/movie"
+	genreAllURI string = "/genre/movie/list"
+
+	sqlHOST string = "127.0.0.1"
+	userName string = "postgres"
+	password string = "admin"
+	port int = 5432
+	db string = "tmdb"
 )
 
+func dbConfigure() string{
+
+	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s",userName,password,sqlHOST,port,db)
+	//return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d ",sqlHOST,userName,password,db,port)
+}
+
 func main(){
+	config := dbConfigure()
+	fmt.Println(config)
+	db, err := gorm.Open(postgres.Open(config),&gorm.Config{
+		//some config here....
+	})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	////////create table
+	db.AutoMigrate(&webCrawler.GenreInfo{}) //generate genreInfo Table
+	db.AutoMigrate(&webCrawler.MovieInfo{}) // generate movieInfo table
+	//
+
+	//TODO - Create all needed genre data with id : with uri:
+	genreAPIUri := host + genreAllURI +"?api_key=" + apiKey + "&language=zh-TW"
+
+
+	//TODO - Insert Data to Database
+	err = webCrawler.GenreTableCreate(genreAPIUri,db)
+	if err != nil{
+		log.Fatalln(err)
+		return
+	}
 
 	var uris []string
-	var res []*webCrawler.APIResult
-	uri := host + upcomingURI +"?api_key=" + apiKey
+	//var res []*webCrawler.APIResult
+	//https://api.themoviedb.org/3/discover/movie?api_key=29570e7acc52b3e085ab46f6a60f0a55&language=zh-TW&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate
+	uri := host + allMovieURI +"?api_key=" + apiKey + "&language=zh-TW&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
 	page := webCrawler.FetchPageInfo(uri)
 	uris = uriGenerator(uri,page)
-	res = webCrawler.FetchMovieInfos(uris)
-	fmt.Println(res[0].MoviesInfo[0].Id)
+
+	//store data to db
+	webCrawler.FetchMovieInfos(uris,db)
+
 }
 
 func uriGenerator(uri string,page int) []string{
--- webCrawler/main.go
+++ webCrawler/main.go
@@ -2,35 +2,56 @@ package webCrawler
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
-
+	"gorm.io/gorm"
 )
+
+/*
+	TODO - Model relationship between movie and genre
+*/
+
 
 var client *http.Client
 
 func init(){
 	client = &http.Client{}
+
 }
 
+var (
+	FetchERRFiled = errors.New("Fetch Err Failed ")
+)
+
+
+//TODO - GETTING API BASE INFO RESPONSE
 type APIResponse struct{
 	Page int `json:"page"`
 	TotalPages int `json:"total_pages"`
 }
 
-type APIResult struct {
+type movieAPIResponse struct {
 	APIResponse
-	MoviesInfo []MovieInfo `json:"results"`
+	Movies []MovieInfo `json:"results"`
 }
 
+type genreAPIResponse struct {
+	Genres []GenreInfo `json:"genres"`
+}
+
+// TODO - Database schema
+
+//MovieInfo TODO - GETTING DATA FROM API
 type MovieInfo struct {
 	Adult            bool    `json:"adult"`
 	BackdropPath     string  `json:"backdrop_path"`
-	GenreIds         []int   `json:"genre_ids"`
-	Id               int     `json:"id"`
+	GenreIds         []int   `json:"genre_ids" gorm:"-"` //we are going to store it with join table ,ignore that...
+	Id               uint    `json:"id" gorm:"primarykey"`
 	OriginalLanguage string  `json:"original_language"`
 	OriginalTitle    string  `json:"original_title"`
 	Overview         string  `json:"overview"`
@@ -41,8 +62,78 @@ type MovieInfo struct {
 	Video            bool    `json:"video"`
 	VoteAverage      float64 `json:"vote_average"`
 	VoteCount        int     `json:"vote_count"`
+	
+	//gorm protocol
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+
+	//Here have many2many relationship
+	//one movie can have many genres
+	//a genres can belong to many result
+
+	GenreInfo []GenreInfo `json:"-" gorm:"many2many:genres_movies"` //json do not contain this info, ignore that
 }
 
+//GenreInfo TODO - Genre data
+type GenreInfo struct {
+	//APIResponse `gorm:"-"` //this info is no need in db
+
+	//genre info
+	Id uint `json:"id" gorm:"primarykey"`
+	Name string `json:"name"`
+
+	////gorm protocol
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
+
+func GenreTableCreate(uri string,db *gorm.DB) error{
+	request, err := http.NewRequest("GET",uri,nil)
+	if err != nil{
+		log.Println(err)
+		return err
+	}
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	res, err := client.Do(request)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer res.Body.Close()
+	fmt.Println(res.Body)
+	body , err := ioutil.ReadAll(res.Body)
+	fmt.Println(len(body))
+	if err != nil{
+		log.Println(err)
+		return err
+	}
+
+	var genres genreAPIResponse
+	err = json.Unmarshal(body, &genres)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	//fmt.Println(len(genres.Genres))
+	// TODO - before inset into database ,need to translate some text to traditional chinese : USE OPEN-CC HERE
+
+
+	var dbGenres []GenreInfo = genres.Genres
+	db.Create(&dbGenres)
+	//
+	for _, genre := range genres.Genres{
+		fmt.Println(genre.Name)
+	}
+	return nil
+}
+
+// FetchPageInfo TODO - just fetching basic information that server is needed...
 func FetchPageInfo(uri string) int{
 	request, err := http.NewRequest("GET", uri, nil)
 	if err != nil {
@@ -69,35 +160,57 @@ func FetchPageInfo(uri string) int{
 	return result.TotalPages //get the total page of current response
 }
 
-func FetchMovieInfos(uris []string) []*APIResult{
-	var results []*APIResult
+//FetchMovieInfos TODO - fetching data from uris list
+func FetchMovieInfos(uris []string,db *gorm.DB) bool{
 	for _,uri := range uris{
-		res := doRequest(uri)
-		results = append(results,res)
+		getMovieFromUri(uri,db) //try it first
 	}
-	return results
+	return true
 }
 
-func doRequest(uri string) *APIResult{
-	var result APIResult
+//getMovieFromUri TODO - getting data from the specific URI
+func getMovieFromUri(uri string,db *gorm.DB){
+	var movieRes movieAPIResponse
 	request , err := http.NewRequest("GET",uri,nil)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
+		return
 	}
+
 	res, err := client.Do(request)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
+		return
 	}
+
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
+		return
 	}
-	err = json.Unmarshal(body,&result)
+
+	err = json.Unmarshal(body,&movieRes)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fmt.Println(result.Page)
-	return &result
+	//for each list has/have a group of genre -> separated it!
+	fmt.Println()
+	for _,moviesInfo := range movieRes.Movies{
+		var currentMovie MovieInfo = moviesInfo
+		var genreIds []int = moviesInfo.GenreIds
+		var genreList []GenreInfo
+		for _,genreId := range genreIds{
+			//for each genre list
+			//let's test
+			curGenre := GenreInfo{
+				Id: uint(genreId),
+			}
+			genreList = append(genreList,curGenre)
+		}
+		currentMovie.GenreInfo = genreList
+		db.Create(&currentMovie)
+		fmt.Println(currentMovie)
+	}
 }
